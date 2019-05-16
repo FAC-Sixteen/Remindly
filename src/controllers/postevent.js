4ab19539ef645eecdc871b01ea428ab000831ffd
seabasshoang <sylviahoang@hotmail.co.uk>
2019-05-16 17:51:56 +0100

    add error handling on post query

--- src/controllers/postevent.js
+++ src/controllers/postevent.js
@@ -3,10 +3,11 @@ const postContact = require('../model/queries/postContact');
 
 const postEvent = (req, res) => {
     const { event_name, event_date, contact_name, phone, group_name, } = req.body;
-
-    postNewEvent(event_name, event_date, contact);
+    console.log(req.body);
+    postNewEvent(event_name, event_date, contact_name);
     postContact(contact_name, phone, group_name)
         .then(res.redirect('/'))
+        .catch(err => console.log(err))
 }
 
 module.exports = postEvent;
--- src/model/queries/postContact.js
+++ src/model/queries/postContact.js
@@ -1,11 +1,28 @@
 const db = require('../db_connection');
 
 const postContact = (name, phone, group_name) => {
-    return db
+    return new Promise((resolve, reject) => {
+        db
         .query(
             "INSERT INTO contacts (name, phone, group_name) VALUES ($1, $2, $3)", [name, phone, group_name]
         )
-        .then(response => response.rows);
-};
-
+        .then(response => {
+            console.log("new contact added to contact table");
+            resolve(true);
+        })
+        .catch(err => reject(err));
+});
+}
 module.exports = postContact;
+
+// const db = require('../db_connection');
+
+// const postContact = (name, phone, group_name) => {
+//     return db
+//         .query(
+//             "INSERT INTO contacts (name, phone, group_name) VALUES ($1, $2, $3)", [name, phone, group_name]
+//         )
+//         .then(response => response.rows);
+// };
+
+// module.exports = postContact;
