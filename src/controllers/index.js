15e866ca9fd8d76f187290f1a8edac1c80444c78
James Levine <jamesalexanderlevine@gmail.com>
2019-05-16 15:06:53 +0100

    added queries to pull dropdowns data

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,14 +1,16 @@
 //modules
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 //initialising router
 const router = express.Router();
 
 //page controllers
-const home = require('./home');
+const home = require("./home");
+const addContact = require("./addContact");
 
 //routes
-router.get('/', home);
+router.get("/", home);
+router.get("/addcontact", addContact);
 
 module.exports = router;
