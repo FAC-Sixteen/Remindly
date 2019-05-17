df31768d791daa691489dc4e0e42e20b262fc155
James Levine <jamesalexanderlevine@gmail.com>
2019-05-17 09:46:37 +0100

    sorted events so that future events are visible

--- src/model/queries/getEvents.js
+++ src/model/queries/getEvents.js
@@ -1,8 +1,31 @@
-const db = require('../db_connection');
+const db = require("../db_connection");
+
+const editObj = obj => {
+  const dateToday = new Date();
+  obj.map(x => {
+    let dateNum = parseInt(
+      JSON.stringify(x.event_date)
+        .slice(6, 11)
+        .replace("-", "")
+    );
+    let todayNum = parseInt(
+      (dateToday.getMonth() + 1)
+        .toString()
+        .concat(dateToday.getDate().toString())
+    );
+    if (dateNum < todayNum) {
+      x.event_monthday = parseInt(x.event_monthday) + 1200;
+    }
+  });
+  return obj.sort(function(a, b) {
+    return a.event_monthday - b.event_monthday;
+  });
+};
+
 const getEvents = () => {
-    return db.query("SELECT * FROM events").then(response => {
-        return response.rows;
-    })
+  return db.query("SELECT * FROM events").then(response => {
+    return editObj(response.rows);
+  });
 };
 
 module.exports = getEvents;
