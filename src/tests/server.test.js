b06117f60b1a09bf28df7060dd94d8492d51bdb1
Burhan Baalwaan <42354400+Baalwaan@users.noreply.github.com>
2019-05-17 16:25:55 +0100

    server.test done() removed from each test

--- src/tests/server.test.js
+++ src/tests/server.test.js
@@ -5,33 +5,29 @@ const app = require('../app');
 describe('jest is working', () => {
     it('should be showing James', () => {
         expect('James').toBe('James');
-        done();
     });
 });
 
 describe('testing home route status code', () => {
-    test('It should respond with status code 200', (done) => {
+    test('It should respond with status code 200', () => {
         request(app).get('/').then((response) => {
             expect(response.statusCode).toBe(200);
-            done();
         });
     });
 });
 
 describe('testing home route content type', () => {
-    test('It should respond with content type text', (done) => {
+    test('It should respond with content type text', () => {
         request(app).get('/').then((response) => {
             expect(response.header["content-type"]).toBe("text/html; charset=utf-8");
-            done();
         });
     });
 });
 
 describe('testing nonexistent endpoint route status code', () => {
-    test('It should respond with status code 404', (done) => {
+    test('It should respond with status code 404', () => {
         request(app).get('/asdgsgsdg').then((response) => {
             expect(response.statusCode).toBe(404);
-            done();
         });
     });
 });
