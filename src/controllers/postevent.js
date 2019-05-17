f7cff953b33c292e03bb92fc2989ead314f09c02
James Levine <jamesalexanderlevine@gmail.com>
2019-05-17 10:37:13 +0100

    fixed eventlist to show dates chronologically

--- src/controllers/postevent.js
+++ src/controllers/postevent.js
@@ -1,13 +1,21 @@
-const postNewEvent = require('../model/queries/postNewEvent');
-const postContact = require('../model/queries/postContact');
+const postNewEvent = require("../model/queries/postNewEvent");
+const postContact = require("../model/queries/postContact");
 
 const postEvent = (req, res) => {
-    const { event_name, event_date, contact_name, phone, group_name, } = req.body;
-    console.log(req.body);
-    postNewEvent(event_name, event_date, contact_name);
-    postContact(contact_name, phone, group_name)
-        .then(res.redirect('/'))
-        .catch(err => console.log(err))
-}
+  const { event_name, event_date, contact_name, phone, group_name } = req.body;
+  console.log(req.body);
+  const event_monthday = JSON.stringify(event_date)
+    .slice(6, 11)
+    .replace("-", "");
+  // console.log(
+  //   JSON.stringify(event_monthday)
+  //     .slice(6, 11)
+  //     .replace("-", "")
+  // );
+  postNewEvent(event_name, event_date, contact_name, event_monthday);
+  postContact(contact_name, phone, group_name)
+    .then(res.redirect("/"))
+    .catch(err => console.log(err));
+};
 
 module.exports = postEvent;
